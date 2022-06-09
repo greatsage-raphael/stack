8f59e146743fc8206c86ddf2e43854a10bf741e2
Collins Lubwama <bizzicole87@gmail.com>
2022-06-09 09:53:03 +0300

    stack parctice 2

---
+++ index.js
@@ -0,0 +1,18 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    push(value) {
+        const newNode = new Node(value) 
+    }
+}
